b9121b00f76e8e87f12097dca54f1bded953a805
kaczo <kaczoreks121@gmail.com>
2024-03-17 17:56:56 +0100

    change cors

--- src/main/java/com/sporttrackback/SportTrackBetaSpringBackendApplication.java
+++ src/main/java/com/sporttrackback/SportTrackBetaSpringBackendApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
     @EnableConfigurationProperties(value = {JwtConfigurationProperties.class})
-@CrossOrigin(origins = {"http://ec2-3-70-127-40.eu-central-1.compute.amazonaws.com:3000","localhost:3000"})
     public class SportTrackBetaSpringBackendApplication {
 
     public static void main(String[] args) {
